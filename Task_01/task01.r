ae79c9538fcbec6a470d7f7343442106f2c0e0af
elg0012 <elg0012@mix.wvu.edu>
2021-01-25 09:49:20 -0500

    emilygreen Task 01 upload b

--- Task_01/task01.r
+++ Task_01/task01.r
@@ -1,2 +1,30 @@
 install.packages("swirl")
 library (swirl)
+library (swirl)
+Emily Green 
+library (swirl)
+Emily Green
+5+7
+x<- 5+7
+x
+y <- x-3
+y
+c(1.1, 9, 3.14)
+z <- c(1.1, 9, 3.14)
+?c
+z <- z, 555, z
+z, 555, z
+z <- z 555 z
+c(z, 555, z)
+z * 2 + 100
+sqrt (z-1) 
+my_sqrt <- sqrt (z-1)
+sqrt (z-1)
+my_sqrt
+my_div <- z/my_sqrt
+my_div
+c (1, 2, 3, 4) + c (0, 10)
+c (1, 2, 3, 4) + c(0, 10, 100)
+my
+elg0012@mix.wvu.edu
+No

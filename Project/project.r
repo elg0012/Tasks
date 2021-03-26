3e855ea65accd9b662cf0d61192a0ce7d3575c3d
elg0012 <elg0012@mix.wvu.edu>
2021-03-25 22:48:39 -0400

    emilygreen Project upload

--- Project/project.r
+++ Project/project.r
@@ -1,3 +1,12 @@
 setwd("~/Desktop/Evolution/Tasks/Project")
 Data <- read.csv("DataSet.csv")
+Data2 <- read.csv("DataSet2.csv")
 Data
+Data2
+Male <- which(Data[,3] == 'M')
+Female <- which(Data[,3] == 'F')
+Region <- (Data[,6])
+TailLength <- (Data[,9])
+Weight <- (Data[,7])
+plot(abs(TailLength), Weight, log="y")
+ 
